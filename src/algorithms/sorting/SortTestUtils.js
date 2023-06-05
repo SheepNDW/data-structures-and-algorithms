be9642caf274ff1f87e5e9d2bde55fd6334a6880
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-06-05 11:33:57 +0800

    refactor: move `testRuntime` into SortTestUtils

--- src/algorithms/sorting/SortTestUtils.js
+++ src/algorithms/sorting/SortTestUtils.js
@@ -1,3 +1,5 @@
+import { shuffle } from '../fisher-yates-shuffle/shuffle';
+
 export const sortedArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 export const reverseArr = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 export const notSortedArr = [15, 8, 5, 12, 10, 1, 16, 9, 11, 7, 20, 3, 2, 6, 17, 18, 4, 13, 14, 19];
@@ -17,4 +19,28 @@ export function swap(array, a, b) {
   array[b] = temp;
   // ES6 destructuring assignment syntax
   // [array[a], array[b]] = [array[b], array[a]];
+}
+
+/**
+ * Test runtime of sorting function
+ * @param {function} sortedFn
+ */
+export function testRuntime(sortedFn) {
+  const array = [];
+  // 向陣列寫入 10000 個資料，其中前 1000 個資料倒序，後 9000 個資料順序
+  for (let i = 0; i < 10000; i++) {
+    if (i < 1000) {
+      array[i] = 1000 - i;
+    } else {
+      array[i] = i;
+    }
+  }
+  console.log('========');
+  let start = new Date() - 0;
+  sortedFn(array);
+  console.log('部分有序的情況', sortedFn.name, new Date() - start);
+  shuffle(array);
+  start = new Date() - 0;
+  sortedFn(array);
+  console.log('完全亂序的情況', sortedFn.name, new Date() - start);
 }
--- src/algorithms/sorting/bubble-sort/BubbleSort.spec.js
+++ src/algorithms/sorting/bubble-sort/BubbleSort.spec.js
@@ -1,7 +1,12 @@
 import { describe, expect, it } from 'vitest';
 import { bubbleSort, bubbleSort1, bubbleSort2, bubbleSort3, cocktailSort } from './BubbleSort';
-import { sortedArr, notSortedArr, negativeArr, negativeArrSorted } from '../SortTestUtils';
-import { shuffle } from '@/algorithms/fisher-yates-shuffle/shuffle';
+import {
+  sortedArr,
+  notSortedArr,
+  negativeArr,
+  negativeArrSorted,
+  testRuntime,
+} from '../SortTestUtils';
 
 describe('Bubble Sort', () => {
   it('should sort array', () => {
@@ -21,31 +26,11 @@ describe('Bubble Sort', () => {
   });
 });
 
-describe.skip('Test Optimized Bubble Sort', () => {
+describe.skip('Test Optimized Bubble Sort runtime', () => {
   it('測試三種 bubble sort 耗時', () => {
-    test(bubbleSort1);
-    test(bubbleSort2);
-    test(bubbleSort3);
-    test(cocktailSort);
+    testRuntime(bubbleSort1);
+    testRuntime(bubbleSort2);
+    testRuntime(bubbleSort3);
+    testRuntime(cocktailSort);
   });
 });
-
-function test(sortedFn) {
-  const array = [];
-  // 向陣列寫入 10000 個資料，其中前 1000 個資料倒序，後 9000 個資料順序
-  for (let i = 0; i < 10000; i++) {
-    if (i < 1000) {
-      array[i] = 1000 - i;
-    } else {
-      array[i] = i;
-    }
-  }
-  console.log('========');
-  let start = new Date() - 0;
-  sortedFn(array);
-  console.log('部分有序的情況', sortedFn.name, new Date() - start);
-  shuffle(array);
-  start = new Date() - 0;
-  sortedFn(array);
-  console.log('完全亂序的情況', sortedFn.name, new Date() - start);
-}
