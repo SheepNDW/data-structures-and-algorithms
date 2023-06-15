bb4eb51a1ef4cad830f08543884a99a12e14b388
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-06-15 15:00:09 +0800

    chore: update quick sort notes and code

--- src/algorithms/sorting/quick-sort/QuickSort.spec.js
+++ src/algorithms/sorting/quick-sort/QuickSort.spec.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { quickSort } from './QuickSort';
+import { quickSort, quickSortSimple, quickSortStack } from './QuickSort';
 import { sortedArr, notSortedArr, negativeArr, negativeArrSorted } from '../SortTestUtils';
 
 describe('QuickSort', () => {
@@ -15,6 +15,42 @@ describe('QuickSort', () => {
     const array = [...negativeArr];
 
     const result = quickSort(array);
+
+    expect(result).toEqual(negativeArrSorted);
+  });
+});
+
+describe('QuickSort Simple', () => {
+  it('should sort array', () => {
+    const array = [...notSortedArr];
+
+    const result = quickSortSimple(array);
+
+    expect(result).toEqual(sortedArr);
+  });
+
+  it('should sort array with negative numbers', () => {
+    const array = [...negativeArr];
+
+    const result = quickSortSimple(array);
+
+    expect(result).toEqual(negativeArrSorted);
+  });
+});
+
+describe('QuickSort Stack', () => {
+  it('should sort array', () => {
+    const array = [...notSortedArr];
+
+    const result = quickSortStack(array);
+
+    expect(result).toEqual(sortedArr);
+  });
+
+  it('should sort array with negative numbers', () => {
+    const array = [...negativeArr];
+
+    const result = quickSortStack(array);
 
     expect(result).toEqual(negativeArrSorted);
   });
