03b00d6a5f25a4e6d760f555a1780a5fd9f8fbe9
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-06-28 18:13:40 +0800

    chore: fix type

--- src/data-structures/linked-list/LinkedList.d.ts
+++ src/data-structures/linked-list/LinkedList.d.ts
@@ -1,10 +1,10 @@
-export declare class Node<T = any> {
+export declare class Node<T> {
   element: T;
   next: Node<T> | null;
   constructor(element: T);
 }
 
-export declare class List<T = any> {
+export declare class List<T = unknown> {
   head: Node<T> | null;
   length: number;
   constructor();
@@ -15,4 +15,38 @@ export declare class List<T = any> {
   findIndex(index: number): Node<T> | null;
   insertAt(index: number, element: T): void;
   removeAt(index: number): void;
+}
+
+export declare class LinkedList<T = unknown> {
+  head: Node<T> | null;
+  length: number;
+  constructor();
+  /**
+   * Adds a new element to the end of the list.
+   */
+  push(element: T): void;
+  /**
+   * Removes the element at the specified position in this list.
+   */
+  removeAt(index: number): T | null;
+  /**
+   * Returns the element at the specified position in this list.
+   */
+  getNodeAt(index: number): Node<T> | null;
+  equalFn(a: T, b: T): boolean;
+  /**
+   * Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+   */
+  indexOf(element: T): number;
+  /**
+   * Removes the first occurrence of the specified element from this list, if it is present.
+   * If the list does not contain the element, it is unchanged.
+   */
+  remove(element: T): T | null;
+  /**
+   * Inserts the specified element at the specified position in this list.
+   */
+  insert(index: number, element: T): boolean;
+  size(): number;
+  isEmpty(): boolean;
 }
