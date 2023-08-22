8e59bfcd6dede12795ea916e6e71dd06305d7106
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-08-22 16:49:31 +0800

    fix: correct insertionSort2 condition
    修正 insertionSort2 函式中的 while 迴圈條件，原先的條件未能正確處理陣列的第一個元素。

--- src/algorithms/sorting/insertion-sort/InsertionSort.js
+++ src/algorithms/sorting/insertion-sort/InsertionSort.js
@@ -3,9 +3,9 @@
  * @param {number[]} array
  */
 function insertionSort(array) {
-  let n = array.length;
+  const n = array.length;
   for (let i = 1; i < n; i++) {
-    let target = array[i];
+    const target = array[i];
     let j;
     // 合併兩個內部迴圈
     for (j = i - 1; j >= 0 && array[j] > target; j--) {
@@ -20,15 +20,15 @@ function insertionSort(array) {
  * @param {number[]} array
  */
 function insertionSort2(array) {
-  let n = array.length;
+  const n = array.length;
   for (let i = 1; i < n; i++) {
-    let target = array[i];
+    const target = array[i];
     let j = i - 1;
-    while (j > 0 && array[j - 1] > target) {
-      array[j] = array[j - 1]; // 前面覆蓋後面
+    while (j >= 0 && array[j] > target) {
+      array[j + 1] = array[j]; // 前面覆蓋後面
       j--;
     }
-    array[j] = target; // 插入目標值
+    array[j + 1] = target; // 插入目標值
   }
 }
 
